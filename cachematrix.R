5122dfb35f6ec80d62630b56ca7518a181505d10
mmascherpa <marco@aphrodite.local>
2014-08-23 10:58:28 -0700

    Added Code
    Added:
    * Commented Code
    * Test Case

--- cachematrix.R
+++ cachematrix.R
@@ -1,3 +1,15 @@
+# ProgrammingAssignment2
+#
+# Tested using:
+# > source('./cachematrix.R')
+# > m<-matrix(runif(4000000,1,1000000),2000,2000)
+# > cm<-makeCacheMatrix(m)
+# > inv<-cacheSolve(cm)
+# calculating inverse matrix
+# > inv<-cacheSolve(cm)
+# getting cached data
+# > 
+#
 # The first function, makeCacheMatrix creates a special "matrix",
 # which is really a list containing a function to
 # set the value of the matrix
@@ -7,6 +19,28 @@
 
 makeCacheMatrix <- function(x = matrix()) {
   
+  # Initialize inverse to null
+  inverse <- NULL
+  
+  # stores the matrix to be inverted into x and resets the inverse to NULL
+  set <- function(y) {
+    x <<- y
+    inverse <<- NULL
+  }
+  
+  # returns the matrix
+  get <- function() x
+  
+  # stores the inverted matrix into inverse
+  setinverse <- function(i) inverse <<- i
+  
+  # returns the inverse matrix (or NULL if it hasn't been calculated yet)
+  getinverse <- function() inverse
+  
+  # makeCacheMatrix return value - a list of methods
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 # This function computes the inverse of the special "matrix"
@@ -15,5 +49,27 @@ makeCacheMatrix <- function(x = matrix()) {
 # cachesolve should retrieve the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  
+  # retrieve the inverse matrix from the cacheMatrix object
+  i <- x$getinverse()
+  
+  # if the inverse matrix is not NULL return it and exit the function
+  if(!is.null(i)) {
+    message("getting cached data")
+    return(i)
+  }
+  
+  message("calculating inverse matrix")
+  
+  # retrieve the matrix from the cacheMatrix object
+  data <- x$get()
+  
+  # invert the matrix
+  i <- solve(data, ...)
+  
+  # store the inverted matrix into the cacheMatrix object
+  x$setinverse(i)
+  
+  # return the inverted matrix
+  i
 }
